91b698a66f9cb56e309e0cd7c3ce192f62ffa85f
Taylor Lee <Taylorlee44@gmail.com>
2019-11-09 01:08:38 -0800

    One more commit

--- Tic-Tac-Toe/Tile.swift
+++ Tic-Tac-Toe/Tile.swift
@@ -1,7 +1,12 @@
 import Foundation
 
 struct Tile {
+    // Becomes true if it is tapped
     var isChosen = false
+    
+    // Initially has a nil value. When tapped, the value is assigned "X" or "O" using isX
     var value: String?
+    
+    // Static variable that alternates between true or false to assign either "X" or "O"
     static var isX = true
 }
